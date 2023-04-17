b1565270886881b8b626bcb9a2eaccc3e2fc59d0
Sam Wasukundi <brosammy1et2@gmail.com>
2023-04-17 23:17:40 +0200

    travail

--- arborescence.php
+++ arborescence.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+    $parents =array(
+            array('enfant1','21','biochimie'),
+            array('enfant2','14','electronique'),
+            array('enfant3','25','pedagogie')
+    );
+
+    foreach($parents as $parent){
+        echo $parent[0].' '.$parent[1].' '.$parent[2]."</br>";
+    }
+?>
+
+<!-- et la si on veut on affiche seulement les options ou encore les ages -->
